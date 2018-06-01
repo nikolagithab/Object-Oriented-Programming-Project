249e81b952a61dc75ad1b9211d9556f4f653e867
Nikola Petrovski <35612001+nikolagithab@users.noreply.github.com>
2018-06-01 00:44:55 -0400

    Update Line.java

--- nikola/Line.java
+++ nikola/Line.java
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+// Line.java
+// =========
+// This class models a line for the "Star" application.
+//
+//  AUTHOR: Nikola Petrovski (petrovsn@sheridancollege.ca)
+// CREATED: 2017-04-18
+// UPDATED: 2017-04-19
+///////////////////////////////////////////////////////////////////////////////
 package nikola;
 
 //  1  to create line class
@@ -6,102 +15,128 @@ public class Line{
 // 2  instance vavrs
     private Vector2 point = new Vector2();
     private Vector2 direction = new Vector2();
-    
-// 3d  ctors he will cover 2 out of 2 handout
-    public Line(float x1, float y1, float x2, float y2){
-        set(x1, y1, x2, y2);
-    }
-    
-// 3c  second contr.. where x = 0 intercept aka (0,b).. 
-//DIRECTION at b is slope:
-//slope m=rise/run.    run is projection....
-//revrite slope in fraction  2=2/1..3/4=3/4/1...0.3=0.3/1  
-    public Line(float slope, float intercept){
-        set(slope, intercept);
-    }
-    
-// 3b  
-    public Line(Vector2 point, Vector2 direction){
-        set(point, direction);
-    }
-    
+
+///////////////////////////////////////////////////////////////////////////////
+// CTORS
+///////////////////////////////////////////////////////////////////////////////
 // 3a  
     public Line(){
-        //
+        point.x = 0.0f;
+        point.y = 0.0f;
+        direction.x = 1.0f;
+        direction.y = 1.0f;
+    }
+// 3b  
+    public Line(Vector2 point, Vector2 direction){
+        this.point = point;
+        this.direction = direction;
+    }
+// 3c 
+    public Line(float slope, float intercept){
+        point.x = 0;
+        point.y = intercept;
+        direction.x = 1;
+        direction.y = intercept + slope;
+    }
+// 3d
+    public Line(float x1, float y1, float x2, float y2){
+        Vector2 v1 = new Vector2(x1, y1);
+        Vector2 v2 = new Vector2(x2, y2);
+        this.point = v1;
+        this.direction = v2;
     }
     
-// 4c  setters..he choose first point..either works
-    public void set(float x1, float y1, float x2, float y2){
-        point.set(x1, y1);//use vector class inplementation see step 4
-        direction.set(x2 - x1, y2 - y1);
+
+///////////////////////////////////////////////////////////////////////////////
+// METHS
+///////////////////////////////////////////////////////////////////////////////
+// 4a  setter.
+    public void set(Vector2 point, Vector2 direction){
+        this.point = point;
+        this.direction = direction;
     }
-    
-// 4b  second setter..slope intercept
+// 4b  setter.
     public void set(float slope, float intercept){
-        point.set(0, intercept);//use 
-        direction.set(1, slope); //when move 1, x/y movements
-    } 
-    
-// 5a  convert standard form to parametric form
-//because or class uses ;point form..
-//abcd are unknown because we do not use coefficients..
-//in vector form: P1 + t v==this is line equation.
-//extract doe x and y components (you can do it) 
-    
-    public void setPoint (Vector2 point){
-        //
+        this.point.x = 0;
+        this.point.y = intercept;
+        this.direction.x = 1;
+        this.direction.y = intercept + slope;
+    }
+// 4c  setter.
+    public void set(float x1, float y1, float x2, float y2){
+        this.point.x = x1;
+        this.point.y = y1;
+        this.direction.x = x2;
+        this.direction.y = y2;
     }
     
+// 5a  vector form and line equation.
+    public void setPoint (Vector2 point){
+        this.point = point;
+    }
 // 5b 
     public Vector2 getPoint(){
-        //
+        return this.point;
     }  
-    
-    
+     
 // 6a  
     public void setDirection(Vector2 direction){
-        //
+        this.direction = direction;
     }
 // 6b  
     public Vector2 getDirection(){
-        //
+        return this.direction;
     }
     
 // 7 
     @Override
     public String toString(){
-        //return "Line... ===... Point: (xy)  Direction: (xy)";
+        String lineParameters = "Line\n" + "====\n" + "Point: (" + this.point.x + ", " + this.point.y + ")\n" + "Direction: (" + this.direction.x + ", " + this.direction.y + ")";
+        return lineParameters;
     }
     
 // 8  find intersection point
     public Vector2 intersect(Line line){//choose better name..
-        //
-        Vector2 point = new Vector2(Float.Nan, Float.Nan, );//parrallel NaN
-        //find coefficients   abcdef
-        float a = direction.y;//Vy
-        float b = -direction.x;//Vx NOTE NEGATIVE
-        float c = direction.y * point.x - direction.x * point.y;
-        
-        float d = line.getDirection().y;//Vy
-        float e = -line.getDirection().x;//Vx NOTE NEGATIVE
-        float f = line.getDirection.y * line.getPoint.x - 
-            line.getDirection.x * line.getPoint.y;//to find all coefficients
-        //just plug them in now
-        
-        
-        //find determinant: ae-bd
-        float det = a * e - b * d;//evaluate parralel..not different!
-        if(det == 0){
-// or if(det != 0) > they intersect>> point.x = (c * e - b * f) / det;
-//point.y = (a * f - c * d) / det;
+        Vector2 intersectPoint = new Vector2(); 
+        if (this.isIntersected(line)){
+            // define vars for standard equation
+            float c = (this.direction.y - this.point.y)*this.point.x - (this.direction.x - this.point.x)*this.point.y; 
+            float a = direction.y - point.y;
+            float b = point.x - direction.x;
+            // line 2
+            float f = (line.direction.y - line.point.y)*line.point.x - (line.direction.x - line.point.x)*line.point.y; ;
+            float d = line.direction.y - line.point.y;
+            float e = line.point.x - line.direction.x;
             
-            return point;//not intersect!   
+            intersectPoint.x = (c*e - b*f) / (a*e - b*d);
+            intersectPoint.y = (a*f - c*d) / (a*e - b*d);
+                        
+        } else {
+            intersectPoint.x = Float.NaN;
+            intersectPoint.y = Float.NaN;
+        }
+        return intersectPoint;   
         }
         
         
 // 9  are lines intersecting?
         public boolean isIntersected(Line line){
-            //
+            // define vars for standard equation
+            // line 1
+            float c = (this.direction.y - this.point.y)*this.point.x - (this.direction.x - this.point.x)*this.point.y; 
+            float a = this.direction.y - this.point.y;
+            float b = this.direction.x - this.point.x;
+                
+            // line2
+            float f = (line.direction.y - line.point.y)*line.point.x - (line.direction.x - line.point.x)*line.point.y; ;
+            float d = line.direction.y - line.point.y;
+            float e = line.direction.x - line.point.x;
+        
+            if ((a*e - b*d) == 0){
+                return false;
+            } else {
+                return true;
+        }
         }
     }
 }
