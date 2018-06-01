a66ac8b2d98b2d69813840a00f2ba9dcbbd75198
Nikola Petrovski <35612001+nikolagithab@users.noreply.github.com>
2018-06-01 00:56:59 -0400

    Update Vector2.java

--- nikola/Vector2.java
+++ nikola/Vector2.java
@@ -1,3 +1,13 @@
+
+///////////////////////////////////////////////////////////////////////////////
+// Vector2.java
+// =========
+// This class models a vector for the "Star" application.
+//
+//  AUTHOR: Nikola Petrovski (petrovsn@sheridancollege.ca)
+// CREATED: 2017-04-18
+// UPDATED: 2017-04-19
+///////////////////////////////////////////////////////////////////////////////
 package nikola;
 
 // 1 to create class
@@ -7,6 +17,9 @@ public class Vector2{
     public float x;
     public float y;
     
+///////////////////////////////////////////////////////////////////////////////
+// CTORS
+///////////////////////////////////////////////////////////////////////////////
 //  3a  to define no-arg constructor
     public Vector2(){
         set(0, 0);
@@ -17,68 +30,67 @@ public class Vector2{
         set(x, y);
     }
     
-//  4b  to define method for initializing the x and y
-//setters..this is reg method with void return type 
-//to reference the hidden data fields of the object being constructed.
+///////////////////////////////////////////////////////////////////////////////
+// METHS
+///////////////////////////////////////////////////////////////////////////////
+//  4a  
+    public void set(Vector2 v){
+        set(v.x, v.y);
+    }
+//  4b
     public void set(float x, float y){
         this.x = x;
         this.y = y;
     }
     
-//  4a  
-    public void set(Vector2 v){
-        set(v.x, v.y);
-    }
-//override toString()..have to have same elements..
-//if not.. then it is a new method!   
-    
+//override toString()..has to have same elements..  
 //  5 to print string
     @Override
     public String toString(){
-        return "Vector2(" + x + "," + y + ")";
+        return "Vector2(" + this.x + "," + this.y + ")";
     }
     
-//  6  make copy..deep copy vs shallow copy..
+//  6  make copy.
     public Vector2 clone(){
-        return new Vector2(x.y);
+        return new Vector2(this.x, this.y);
     }
     
 // 7  method for result of vector addition..aritmetics
     public Vector2 add(Vector2 v){
-        x += v.x;//to reduce> x = x + v.x
-        y += v.y;//singular scallar value!
+        this.x += v.x;//to reduce> x = x + v.x
+        this.y += v.y;//singular scallar value!
         return this;//it return itself so you can keep adding.
     }
     
 // 8  to subtruct vectors
     public Vector2 subtract(Vector2 v){
-        x -= v.x;//
-        y -= v.y;//
-        return this;//
+        this.x -= v.x;
+        this.y -= v.y;
+        return this;
     }
     
 // 9 to multiply scalar with vector
     public Vector2 scale(float scalar){
-        //
-        return this;//
+        this.x *= 2;
+        this.y *= 2;
+        return this;
     }
 // 10  dot product
     public float dot(Vector2 v){
-        return (x * v.x) + (y * v.y);
+        return (this.x * v.x) + (this.y * v.y);
     }
     
 // 11  math getlength.
     public float getLength() {
-        return Math.sqrt(thix.x * this.x + this.y * this.y);
-        float d = getLength();//
-        x /= d;//
-        y /= d;
+        return (float)Math.sqrt(thix.x * this.x + this.y * this.y);
     }
     
 //  12  to make vector with unit length and return result.
     public Vector2 normalize() {
-        return new Vector2((this.x/Math.sqrt(x * x + y * y)),
-                           (this.y/Math.sqrt(x * x + y * y)));//
+        float d = getLength();
+        x /= d;
+        y /= d;
+        return this;
     }
     
 }
